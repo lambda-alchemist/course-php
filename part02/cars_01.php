31d05a2b1070e81126784e5a5e1960a30641ab51
Guilherme M. Miranda <alchemist.software@proton.me>
2022-07-22 15:43:33 -0300

    added cars

---
+++ part02/cars_01.php
@@ -0,0 +1,25 @@
+<?php
+
+$cars = array("ferrari", "audi", "lambo", "chrysler", "tesla", "corola", "civic", "bmw", "impala68", "mercedes");
+
+sort($cars);
+
+foreach($cars as $key => $value){
+	switch($key){
+	case(0):  $extra = "Is the zeroth."; break;
+	case(1):  $extra = "Spelled \"BMW\"."; break;
+	case(2):  $extra = "Unheard of."; break;
+	case(3):  $extra = "It has five letters in the name."; break;
+	case(4):  $extra = "It has four wheels."; break;
+	case(5):  $extra = "Ferrari"; break;
+	case(6):  $extra = "Made before the moonlanding."; break;
+	case(7):  $extra = "Rich people status symbol."; break;
+	case(8):  $extra = "INvolved in th holocaust."; break;
+	case(9):  $extra = "Should be cheaper."; break;
+	}
+	echo "cars[$key] = $value => $extra\n";
+};
+
+
+
+?>
