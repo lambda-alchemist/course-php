ba16e7e4e10141f553ba4aade2c720b57be5d04b
Guilherme M. Miranda <alchemist.software@proton.me>
2022-07-26 17:33:30 -0300

    missing file readded

---
+++ part03/illness_01.php
@@ -0,0 +1,33 @@
+<?php
+
+$pat = array(
+	"Guilherme", "João",
+	"Neto", "Pedro",
+	"Isaac", "Gabriel",
+	"Clodoaudo", "Iago",
+	"Paula", "Gabriela",
+	"Maria", "Silva",
+	"Joseph", "Linus",
+	"Michael", "American",
+	"Steve", "William",
+	"James", "Arnold",
+);
+
+$con = array(
+	"AIDS", "Skin Cancer",
+	"Videogame addiction", "Meningitis",
+	"Dengue", "Gonorrhea",
+	"Kidney stone", "COVID-19",
+	"Food poisoning", "Radioactive exposure",
+	"Alcohol poisoning", "Foreign object insertion",
+	"Python programmer", "Stress induced hypertension",
+	"Lung cancer", "UTI",
+	"Drug overdose", "Car accident injury",
+	"GSW", "Gym accident",
+);
+
+foreach ( $pat as $key => $val ) {
+	echo "Patient: $val > $con[$key]\n";
+};
+
+?>
