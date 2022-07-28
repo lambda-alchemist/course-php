6aeb060e7a24e84144b0369967f11f866320954a
Guilherme M. Miranda <alchemist.software@proton.me>
2022-07-28 16:46:00 -0300

    part 3: work is done

--- part03/work_01.php
+++ part03/work_01.php
@@ -1,5 +1,21 @@
 <?php
 
+$totalHours = 40;
+$extraHours = 10;
+$hourlyRate = 15.00;
+$salary = 0;
 
+if($totalHours <= 40) {
+	$salary =
+		$totalHours * $hourlyRate; }
+else
+	
+if($totalHours >= 40) {
+$salary =
+	($hourlyRate * 40) +
+	($extraHours * ($hourlyRate * 1.5));
+}
+
+echo "The final salary is USD\$$salary\n";
 
 ?>
