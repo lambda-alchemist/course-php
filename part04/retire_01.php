e1114245461b784a8d0dc8f87cd90748016babec
Guilherme M. Miranda <alchemist.software@proton.me>
2022-07-29 16:36:23 -0300

    Part 4: minor fix

--- part04/retire_01.php
+++ part04/retire_01.php
@@ -3,7 +3,7 @@
 class someWorker {
 	private $age;
 	private $exp;
-
+	
 	public function setAge($x) { $this->age = $x; }
 	public function setExp($y) { $this->exp = $y; }
 
@@ -13,9 +13,9 @@ class someWorker {
 
 $neto = new someWorker;
 
-$neto->setAge(60);
-$neto->setExp(30);
-
+$neto->setAge(50);
+$neto->setExp(10);
+$retireStatus = 0;
 $worker = array(
 	$neto->getAge(),
 	$neto->getExp()
@@ -23,11 +23,14 @@ $worker = array(
 
 if ($worker[0] >= 65)
 	{ $retireStatus = 1; }
-	
+
 if ($worker[1] >= 30)
 	{ $retireStatus = 1; }
 
 if ($worker[0] >= 60 && $worker[1] >= 25)
 	{ $retireStatus = 1; }
+
+if ($retireStatus)  { echo "The worker Neto is able to retire\n"; }
+if (!$retireStatus) { echo "The worker Neto can't retire\n"; }
 
 ?>
